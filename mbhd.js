73fd02b47779309a4af337563aeba604b06083b0
John Johnston <johnjohnston@mac.com>
2018-04-24 20:37:08 +0100

    Update mbhd.js

--- mbhd.js
+++ mbhd.js
@@ -32,7 +32,7 @@ runthis();
 
 
 var huffdufferclick=function(u){
-	//alert(u);
+
 	window.open('https://huffduffer.com/add?popup=true&page='+u,'huffduff','scrollbars=1,status=0,resizable=1,location=0,toolbar=0,width=360,height=480');
 
 }
