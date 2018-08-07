d0a3f6794a1f6fbf38300c342a6565de2a44ae08
John Johnston <johnjohnston@mac.com>
2018-08-07 13:06:44 +0100

    Update mbhd.js

--- mbhd.js
+++ mbhd.js
@@ -28,6 +28,7 @@ if (location.href=='https://micro.blog/discover/podcasts'){
 runthis();
 }else{
 	 var w=window.open('https://huffduffer.com/add?popup=true&page='+encodeURIComponent(location.href),'huffduff','scrollbars=1,status=0,resizable=1,location=0,toolbar=0,width=360,height=480');
+console.log(w);
 }
 
 
